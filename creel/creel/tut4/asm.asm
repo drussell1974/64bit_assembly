b4dfce8253047b67735383c9a105ca09c153da35
drussell1974 <dave@jazzthecat.co.uk>
2021-06-04 11:10:00 +0100

    create mov tutorial

--- creel/creel/tut4/asm.asm
+++ creel/creel/tut4/asm.asm
@@ -1,32 +1,30 @@
 global _start:
 section .data ; variables
-    myByte db 90
-    myInt dw 91
+    myByte db 89
 
 section .text ; instructions
 _start:
-    mov rax, 89         ; immediate into the accumulator
-    mov cx, ax          ; register to register
-    mov cx, eax         ; but this is illegal as it should be the same size
-    mov myByte, r11     ; memory to register 
-    mov r11, myByte     ; register to memory
-    mov myByte, 92      ; immediate to memory
-    mov myByte, myInt   ; but this is illegal as you cannot move memory to memory
+    mov rax, 89         ; OK immediate into the accumulator - 
+    mov cx, ax          ; OK register to register
+    mov r11, [myByte]     ; OK memory to register
+    ; THESE ARE ILLEGAL
+    ; mov cx, eax         ; it should be the same size
+    ; mov myByte, r11     ; you cannot have memory to register 
+    ; mov myByte, 92      ; you cannot have immediate to memory
+    ; mov myByte, myInt   ; you cannot move memory to memory
 
 ; MOV src, dest
 ; DEBUG
 ; break _start
 ; run
+; ni
 ; info register rax
+; rax            0x59                89
 ; ni
 ; info register cx
-; ni -- illegal
-; info register myByte
+; cx             0x59                89
 ; ni
-; info register myByte
-; ni
-; info register myByte
-
-
+; info register r11
+; r11            0x100000059         4294967385
 
  
